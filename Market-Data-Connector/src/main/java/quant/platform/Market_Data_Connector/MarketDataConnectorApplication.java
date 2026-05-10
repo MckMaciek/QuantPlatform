5af3557e737486e4cf417d0c10ce0ccc490391e7
Maciej <mckmusial@gmail.com>
2026-05-10 21:44:28 +0200

    Connector init

---
+++ Market-Data-Connector/src/main/java/quant/platform/Market_Data_Connector/MarketDataConnectorApplication.java
@@ -0,0 +1,12 @@
+package quant.platform.Market_Data_Connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketDataConnectorApplication {
+
+    static void main(final String[] args) {
+        SpringApplication.run(MarketDataConnectorApplication.class, args);
+    }
+}
---
+++ Market-Data-Connector/src/test/java/quant/platform/Market_Data_Connector/MarketDataConnectorApplicationTests.java
@@ -0,0 +1,12 @@
+package quant.platform.Market_Data_Connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDataConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
