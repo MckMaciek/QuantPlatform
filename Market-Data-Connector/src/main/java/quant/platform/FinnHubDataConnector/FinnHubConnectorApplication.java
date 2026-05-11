f0d55353b8f9ad12cc74fac5932ed82f1b4cdab4
Maciej <mckmusial@gmail.com>
2026-05-11 20:11:48 +0200

    Connector init

--- Market-Data-Connector/src/main/java/quant/platform/FinnHubDataConnector/FinnHubConnectorApplication.java
+++ Market-Data-Connector/src/main/java/quant/platform/FinnHubDataConnector/FinnHubConnectorApplication.java
@@ -1,12 +1,12 @@
-package quant.platform.MarketDataConnector;
+package quant.platform.FinnHubDataConnector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MarketDataConnectorApplication {
+public class FinnHubConnectorApplication {
 
     static void main(final String[] args) {
-        SpringApplication.run(MarketDataConnectorApplication.class, args);
+        SpringApplication.run(FinnHubConnectorApplication.class, args);
     }
 }
--- Market-Data-Connector/src/test/java/quant/platform/FinnHubDataConnector/FinnHubConnectorApplicationTests.java
+++ Market-Data-Connector/src/test/java/quant/platform/FinnHubDataConnector/FinnHubConnectorApplicationTests.java
@@ -1,10 +1,10 @@
-package quant.platform.MarketDataConnector;
+package quant.platform.FinnHubDataConnector;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MarketDataConnectorApplicationTests {
+class FinnHubConnectorApplicationTests {
 
 	@Test
 	void contextLoads() {
