ada970cb4712a221bce32a3496b7d7da27ddfc94
Maciej <mckmusial@gmail.com>
2026-05-18 00:11:28 +0200

    Connector init

---
+++ FinnHub-Data-Connector/src/main/java/quant/platform/FinnHubDataConnector/config/AppConfig.java
@@ -0,0 +1,20 @@
+package quant.platform.FinnHubDataConnector.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+@Configuration
+@EnableScheduling
+public class AppConfig {
+
+    @Bean
+    public ScheduledExecutorService scheduledExecutorService() {
+        return Executors.newScheduledThreadPool(0,
+                Thread.ofVirtual().name("virtual-websocket-")
+                        .factory());
+    }
+}
