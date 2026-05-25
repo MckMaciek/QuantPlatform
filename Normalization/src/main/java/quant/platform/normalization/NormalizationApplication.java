d111147a15032fa99024b2a5296688eee8f934eb
Maciej <mckmusial@gmail.com>
2026-05-25 21:58:17 +0200

    normalization-service-init

---
+++ Normalization/src/main/java/quant/platform/normalization/NormalizationApplication.java
@@ -0,0 +1,11 @@
+package quant.platform.normalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormalizationApplication {
+    static void main(final String[] args) {
+        SpringApplication.run(NormalizationApplication.class, args);
+    }
+}
