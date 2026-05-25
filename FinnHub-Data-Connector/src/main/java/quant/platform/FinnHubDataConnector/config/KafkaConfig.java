e166134299819bf79c7484940fe3222c5075b2f6
Maciej <mckmusial@gmail.com>
2026-05-25 21:50:09 +0200

    Connector init

--- FinnHub-Data-Connector/src/main/java/quant/platform/FinnHubDataConnector/config/KafkaConfig.java
+++ FinnHub-Data-Connector/src/main/java/quant/platform/FinnHubDataConnector/config/KafkaConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class KafkaConfig {
 
-    public static final String TOPIC_NAME = "finnhub-io-raw-";
+    public static final String TOPIC_NAME = "raw.finnhub.market-";
 
     private final String serviceId;
 
