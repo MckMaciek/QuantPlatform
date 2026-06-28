f20c653fbd4e0bc534ca08aee1b2c770800d6846
Maciej <mckmusial@gmail.com>
2026-06-28 21:32:35 +0200

    normalization-service-init

--- Normalization/src/main/java/quant/platform/normalization/finnhub/RawFinnHubTradeListener.java
+++ Normalization/src/main/java/quant/platform/normalization/finnhub/RawFinnHubTradeListener.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import quant.platform.normalization.normalized.api.NormalizedTrade;
+import quant.platform.normalization.normalized.deduplication.TradeDeduplicator;
 import quant.platform.normalization.normalized.publisher.NormalizedCommonTradePublisher;
 
 import java.util.List;
@@ -14,16 +15,21 @@ import java.util.List;
 @RequiredArgsConstructor
 class RawFinnHubTradeListener {
 
+    private final RawFinnHubNormalizer rawFinnHubNormalizer = new RawFinnHubNormalizer();
+
+    private final TradeDeduplicator tradeDeduplicator;
+
     private final NormalizedCommonTradePublisher commonPublisher;
     private final NormalizedFinnHubTradePublisher finHubPublisher;
-
-    private final RawFinnHubNormalizer rawFinnHubNormalizer = new RawFinnHubNormalizer();
 
     @SuppressWarnings("unused")
     @KafkaListener(topics = "${kafka.topics.raw-finnhub}", groupId = "${spring.kafka.consumer.group-id}")
     void onMessage(final RawFinnHubTrade trade) {
         log.info("Received FinnHub trade message, records: {}", trade.data().size());
-        final List<NormalizedTrade> normalized = rawFinnHubNormalizer.normalize(trade);
+        final List<NormalizedTrade> normalized = rawFinnHubNormalizer.normalize(trade)
+                .stream()
+                .filter(tradeDeduplicator::isDuplicate)
+                .toList();
 
         normalized.forEach(commonPublisher::publish);
         normalized.forEach(finHubPublisher::publish);
--- Normalization/src/main/java/quant/platform/normalization/normalized/deduplication/TradeDeduplicator.java
+++ Normalization/src/main/java/quant/platform/normalization/normalized/deduplication/TradeDeduplicator.java
@@ -6,5 +6,14 @@ import quant.platform.normalization.normalized.api.NormalizedTrade;
  * This interface should be abstract, and should not leak storage used under the hood.
  */
 public interface TradeDeduplicator {
+
+    /**
+     * The same trade can be potentially already processed i.e. by different service instance etc.
+     * Thus need to check, whether current trade is a duplicate or not.
+     * Using this service guarantees exactly once delivery semantics.
+     *
+     * @param trade normalized trade.
+     * @return true in case the trade has been already processed, false otherwise.
+     */
     boolean isDuplicate(final NormalizedTrade trade);
 }
